a40693b493be313361cf4b556133585c7d37adf7
Miguel de la Cruz <miguel@mcrx.me>
2020-04-30 16:54:04 +0200

    Defer campaign loading in the wrappers

--- cmd/root.go
+++ cmd/root.go
@@ -10,25 +10,25 @@ import (
 )
 
 func withApp(f func(*app.App, *cobra.Command, []string)) func(*cobra.Command, []string) {
-	a, err := app.NewApp("./campaign.json")
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "ERROR: "+err.Error())
-		os.Exit(1)
-	}
-
 	return func(cmd *cobra.Command, args []string) {
+		a, err := app.NewApp("./campaign.json")
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "ERROR: "+err.Error())
+			os.Exit(1)
+		}
+
 		f(a, cmd, args)
 	}
 }
 
 func withAppE(f func(*app.App, *cobra.Command, []string) error) func(*cobra.Command, []string) error {
-	a, err := app.NewApp("./campaign.json")
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "ERROR: "+err.Error())
-		os.Exit(1)
-	}
-
 	return func(cmd *cobra.Command, args []string) error {
+		a, err := app.NewApp("./campaign.json")
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "ERROR: "+err.Error())
+			os.Exit(1)
+		}
+
 		return f(a, cmd, args)
 	}
 }
