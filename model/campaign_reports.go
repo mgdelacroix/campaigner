8651151fd3706534d77e8b0df98a35e87dfb880f
Ben Schumacher <ben.schumacher@mattermost.com>
2024-11-27 16:43:58 +0100

    Order output of campaigner report users command (#12)
    * Order output of campaigner report users command
    
    * Update

--- model/campaign_reports.go
+++ model/campaign_reports.go
@@ -3,11 +3,32 @@ package model
 import (
 	"fmt"
 	"os"
+	"sort"
 	"text/tabwriter"
 )
 
+type Contibutor struct {
+	Username      string
+	Contributions int
+}
+
 func (c *Campaign) PrintUserReport() {
-	userTickets := map[string]int{}
+	contributors := c.Contritutors()
+
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 3, ' ', tabwriter.AlignRight)
+	fmt.Fprintln(w, "GitHub username\tTickets closed\t")
+	fmt.Fprintln(w, "---------------\t--------------\t")
+
+	for _, c := range contributors {
+		fmt.Fprintf(w, "%s\t%d\t\n", c.Username, c.Contributions)
+	}
+
+	w.Flush()
+	fmt.Println()
+}
+
+func (c *Campaign) Contritutors() []Contibutor {
+	contributions := map[string]int{}
 	for _, ticket := range c.Tickets {
 		if !ticket.IsClosed() {
 			continue
@@ -15,21 +36,21 @@ func (c *Campaign) PrintUserReport() {
 
 		user := ticket.GithubAssignee
 		if user != "" {
-			if count, ok := userTickets[user]; ok {
-				userTickets[user] = count + 1
+			if count, ok := contributions[user]; ok {
+				contributions[user] = count + 1
 			} else {
-				userTickets[user] = 1
+				contributions[user] = 1
 			}
 		}
 	}
 
-	fmt.Println()
-	w := tabwriter.NewWriter(os.Stdout, 0, 0, 3, ' ', tabwriter.AlignRight)
-	fmt.Fprintln(w, "GitHub username\tTickets closed\t")
-	fmt.Fprintln(w, "---------------\t--------------\t")
-	for user, count := range userTickets {
-		fmt.Fprintf(w, "%s\t%d\t\n", user, count)
+	contributors := make([]Contibutor, 0, len(contributions))
+	for user, count := range contributions {
+		contributors = append(contributors, Contibutor{Username: user, Contributions: count})
 	}
-	w.Flush()
-	fmt.Println()
+	sort.Slice(contributors, func(i, j int) bool {
+		return contributors[i].Contributions > contributors[j].Contributions
+	})
+
+	return contributors
 }
